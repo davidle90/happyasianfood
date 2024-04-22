a4ab0ac51313c1d4e7cfc105d44fc64fd6050882
poolexdee <davidle90@hotmail.com>
2024-04-23 01:59:02 +0200

    style header and index

--- resources/views/pages/public/index.blade.php
+++ resources/views/pages/public/index.blade.php
@@ -1,27 +1,22 @@
 @extends('layouts.public.main')
 
 @section('styles')
+    <style>
+        .haf-bg-img {
+            background-image: url('{{ asset('/images/hero-placeholder.jpg') }}');
+        }
+    </style>
 @endsection
 
 @section('modals')
 @endsection
 
 @section('content')
-    <div class="container mx-auto">
-        <div class="w-4/5 mx-auto my-12 py-12">
-            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
-                Happy Asian Food
-            </h1>
-            <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-                This is a template for future projects - using Tailwind CSS, Fontawesome / Bootstrap Icons and DropzoneJS. Includes public and admin pages.
-            </p>
-            <div class="text-center">
-                <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Learn more
-                    <i class="fa-solid fa-arrow-right ml-2"></i>
-                </a>
-            </div>
-        </div>
+    <div class="flex items-center justify-center bg-fixed bg-parallax bg-cover h-96 haf-bg-img">
+        <h1 class="text-5xl font-bold haf-font">Happy Asian Food</h1>
+    </div>
+    <div class="mt-10">
+        <h1 class="text-3xl font-bold text-center">Meny</h1>
     </div>
 @endsection
 

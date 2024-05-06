43b1d87b6be8094ff0ca3dcaa47e97f3b1bd219e
davidle90 <89838376+davidle90@users.noreply.github.com>
2024-05-06 17:57:55 +0200

    add logo to admin sidebar

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -8,7 +8,7 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <a href="{{ route('admin.index') }}" class="flex items-center ps-2.5 mb-5">
-            <img src="#" class="h-6 me-3 sm:h-7" alt="logo" />
+            <img src="{{ asset('/images/HappyAsianFood.png') }}" class="h-6 me-3 sm:h-7" alt="logo" />
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ config('app.name', 'Laravel') }}</span>
         </a>
         <ul class="space-y-2 font-medium">

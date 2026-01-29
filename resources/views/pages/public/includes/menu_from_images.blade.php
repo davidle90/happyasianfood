43f01b4be0ed55dae0a13fec1f36d5345d4528d9
davidle90 <davidle90@hotmail.com>
2026-01-29 19:04:26 +0100

    Reorder menu images in menu_from_images.blade.php
    update menu url

--- resources/views/pages/public/includes/menu_from_images.blade.php
+++ resources/views/pages/public/includes/menu_from_images.blade.php
@@ -2,7 +2,7 @@
     <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m10.jpg" alt="menu_1">
     <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m20.jpg" alt="menu_2">
     <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m34.jpg" alt="menu_3">
-    <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m34.jpg" alt="menu_4">
-    <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m50.jpg" alt="menu_5">
-    <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m55.jpg" alt="menu_6">
+    <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m50.jpg" alt="menu_4">
+    <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m55.jpg" alt="menu_5">
+    <img class="mb-2 border-brand" src="https://dl-spaces.ams3.cdn.digitaloceanspaces.com/happyasianfood/menu/m60.jpg" alt="menu_6">
 </div>
